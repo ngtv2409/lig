d2943847251c218d96a2d71128d436c6c9681f6b
ngtv <tuanvy860@gmail.com>
2026-01-01 19:56:49 +0700

    feat : add basic output formatting

---
+++ src/out.rs
@@ -0,0 +1,53 @@
+use crate::search::{Line};
+use std::collections::HashMap;
+
+pub struct OutOptions {
+    pub show_filename : bool,
+    pub show_linenumber : bool,
+    pub show_colnumber : bool
+}
+impl Default for OutOptions {
+    fn default() -> Self {
+        Self {
+            show_filename: false,
+            show_linenumber: false,
+            show_colnumber: false
+        }
+    }
+}
+
+pub fn print_matches(pats : &HashMap<String, Vec<Line>>, opts : &OutOptions) {
+    for (pat, lines) in pats.iter() {
+        println!("{} ({} matches)", pat, lines.len());
+        for line in lines {
+            for m in &line.matches {
+                println!("    {}{}",
+                    format_location(&line.filename, line.lineno, 
+                        byte_to_char_offset(&line.line, m.moffbeg), &opts),
+                    line.line
+                );
+            }
+        }
+    }
+}
+
+fn byte_to_char_offset(s: &str, byte_offset: usize) -> usize {
+    s[..byte_offset].chars().count()
+}
+
+fn format_location(filename : &str, lineno : usize, colno : usize,
+                   opts : &OutOptions) -> String {
+    let mut s : String = String::new();
+
+    if opts.show_filename {
+        s += &format!("{}:", filename);
+    }
+    if opts.show_linenumber {
+        s += &format!("{}:", lineno + 1);
+    }
+    if opts.show_colnumber {
+        s += &format!("{}:", colno + 1);
+    }
+
+    s
+}
