29ba2b20eaf45e318ba38880d23d3e816eddb62e
ngtv <tuanvy860@gmail.com>
2026-01-01 18:31:28 +0700

    feat : change architecture. We now search into Line containing matches

--- src/search.rs
+++ src/search.rs
@@ -9,42 +9,56 @@ use std::collections::HashMap;
 
 pub type PatternMap = HashMap<String, Regex>;
 
-/* An association of filename and matches */
-#[derive(Debug)]
-pub struct FileMatch{
-    pub filename : String, pub match : Match
-};
-
+#[allow(dead_code)]
 #[derive(Debug)]
 pub struct Match {
-    pub line     : String, // lile string
-    pub lineno   : usize,  // line number in file
     pub moffbeg  : usize,  // the line start byte offset of match
     pub moffend  : usize,  // the line end byte offset of match (exclusive)
 
     pub patname  : String  // name of the pattern matched   
 }
 
+#[derive(Debug)]
+pub struct Line {
+    pub line    : String,
+    pub lineno  : usize,
+
+    pub matches : Vec<Match>
+}
+
+/* An association of filename and matches */
+
+pub struct FileMatch {
+    pub filename : String, pub lines : Vec<Line>
+}
+
+
 /*
     Promise: Return vector is sorted by lineno
 */
-pub fn match_file<F : BufRead>(reader : F, patterns : &PatternMap) -> Vec<Match> {
-    let mut ms : Vec<Match> = Vec::new();
+pub fn match_file<F : BufRead>(reader : F, patterns : &PatternMap) -> Vec<Line> {
+    let mut linests : Vec<Line> = Vec::new();
 
     for (lineno, line) in reader.lines().enumerate() {
         let line = line.expect("Failed to read line");
+        let mut linest : Line = Line {
+            line    : line.clone(),
+            lineno  : lineno,
+            matches : Vec::<Match>::new()
+        };
         for (patn, re) in patterns {
             for m in re.find_iter(&line) {
-                ms.push(Match {
-                    line    : line.clone(),
-                    lineno  : lineno,
+                linest.matches.push(Match {
                     moffbeg : m.start(),
                     moffend : m.end(),
                     patname : patn.clone()
                 });
             }
         }
+        if linest.matches.len() > 0 {
+            linests.push(linest);
+        }
     }
 
-    ms
+    linests
 }
